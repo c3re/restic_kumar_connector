ab7b11c495698c30c44d9a209f1e33c68000742e
Dirk Heilig <heilig@adesso-mobile.de>
2025-09-09 12:24:56 +0200

    fix typo

--- htdocs/json.php
+++ htdocs/json.php
@@ -21,7 +21,7 @@ if(!count($resticData)){
 }
 $backups=[];
 foreach($resticData as $snapshot){
-    $id=$snapshot['hostnamer'] . "|" . implode(",",$snapshot['paths']);
+    $id=$snapshot['hostname'] . "|" . implode(",",$snapshot['paths']);
     if(!isset($backups[$id])){
         $backups[$id]=0;
     }
@@ -37,25 +37,3 @@ file_put_contents(
     "/var/www/data/$fileName",
     json_encode($backups, JSON_UNESCAPED_SLASHES + JSON_PRETTY_PRINT)
 );
-
-
-
-/*
-
-    [0] => Array
-        (
-            [time] => 2023-12-31T01:00:06.98861049+01:00
-            [parent] => 4043eca318f979c0b94c3c999b7834c9a3a8b7db42b6b325142aaf8d6aa54bae
-            [tree] => 850bd58eff83ffb70b9d73803b65bd8160ad678e5d98724aca1c00c0f7590c25
-            [paths] => Array
-                (
-                    [0] => /var/lib/docker/volumes
-                )
-
-            [hostname] => wau01.c3re.de
-            [username] => root
-            [id] => 0430b227b440199afa309bb6d41449abc494a64d57894a2a68d225056d008c30
-            [short_id] => 0430b227
-        )
-
-*/
